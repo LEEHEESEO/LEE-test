ad1db3e57746f2d25de8c3437de8e744be4446b4
LEEHEESEO <107111970+LEEHEESEO@users.noreply.github.com>
2023-03-10 13:22:20 +0900

    Add files via upload

---
+++ note.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int numbers[10];
+    int i, j, temp;
+
+    // Read in 10 numbers from the user
+    printf("Enter 10 numbers:\n");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &numbers[i]);
+    }
+
+    // Sort the numbers using bubble sort
+    for (i = 0; i < 9; i++) {
+        for (j = 0; j < 9 - i; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                temp = numbers[j];
+                numbers[j] = numbers[j + 1];
+                numbers[j + 1] = temp;
+            }
+        }
+    }
+
+    // Print the sorted numbers
+    printf("Sorted numbers in ascending order:\n");
+    for (i = 0; i < 10; i++) {
+        printf("%d\n", numbers[i]);
+    }
+
+    return 0;
+}
